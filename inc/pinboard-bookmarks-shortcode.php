48b326c44e103f3f67e9aa0e89fc6800061cd361
aldolat <aldolat@gmail.com>
2017-04-29 14:14:29 +0200

    Updated shortcode options

--- inc/pinboard-bookmarks-shortcode.php
+++ inc/pinboard-bookmarks-shortcode.php
@@ -24,21 +24,26 @@ function pinboard_bookmarks_sc( $atts ) {
 	extract( shortcode_atts( array(
         'username'         => '',
         'tags'             => '',
+        'source'           => '', // This is the source in Pinboard, like 'from:pocket', 'from:instapaper', or 'from:twitter'.
 		'quantity'         => 5,
 		'random'           => false,
 		'display_desc'     => false,
 		'truncate'         => 0,
 		'display_date'     => false,
+        'display_time'     => false,
 		'date_text'        => esc_html__( 'Stored on:', 'pinboard-bookmarks' ),
 		'display_tags'     => false,
 		'tags_text'        => esc_html__( 'Tags:', 'pinboard-bookmarks' ),
 		'display_hashtag'  => true,
+        'use_comma'        => false,
 		'display_arrow'    => false,
 		'display_archive'  => true,
 		'archive_text'     => esc_html__( 'See the bookmarks on Pinboard', 'pinboard-bookmarks' ),
+        'list_type'        => 'bullet',
 		'display_arch_arr' => true,
 		'new_tab'          => false,
 		'nofollow'         => true,
+        'admin_only'       => true,
         'debug_options'    => false,
         'debug_urls'       => false
 	), $atts ) );
